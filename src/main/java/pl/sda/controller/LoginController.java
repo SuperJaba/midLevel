0dff64f00c5cee5458ce4635175347a53244a6e1
SuperJaba <duneval@gmail.com>
2017-03-15 13:23:35 +0100

    Fixed issue with NonPointerException - wrong passwordField name in LoginController class

--- src/main/java/pl/sda/controller/LoginController.java
+++ src/main/java/pl/sda/controller/LoginController.java
@@ -1,4 +1,5 @@
 package pl.sda.controller;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
@@ -11,49 +12,44 @@ import pl.sda.service.UserService;
  * Created by RENT on 2017-03-14.
  */
 
-    public class LoginController {
+public class LoginController {
 
-        @FXML
-        private TextField passwordTextField;
+    @FXML
+    private TextField passwordField;
 
-        @FXML
-        private Button submit;
+    @FXML
+    private Button submit;
 
-        @FXML
-        private TextField loginTextField;
+    @FXML
+    private TextField loginTextField;
 
-        @FXML
-        void submitOnAction(ActionEvent event) {
+    @FXML
+    void submitOnAction(ActionEvent event) {
 //            System.out.println(loginTextField.getText());
-//            System.out.println(passwordTextField.getText());
-            UserService userService = new UserService();
-           User user = new User (loginTextField.getText(), passwordTextField.getText());
+//            System.out.println(passwordTextField.getText().getCause());
+        UserService userService = new UserService();
+        User user = new User(loginTextField.getText(), passwordField.getText());
 //            userField.setLogin(loginTextField.getText());
-            //linijka powyzej wpisana bezposrednio w konstruktor User userField
-            Alert alert1 = new Alert(Alert.AlertType.INFORMATION, "Hello");
-            Alert alert2 = new Alert(Alert.AlertType.INFORMATION, "Hello");
-            userService.authenticatie(user);
-            if () {
-
-                alert1.show();
-            } else {
-                alert2.show();
-
-            }
-
-
-
-
-
-            //napisac if'a czy login sie zgadza i czy pass dziala! wyswietlac komunikaty w odpowiednich przypadkach
-            /**
-             * 1. powolac do zycia UserService
-             * 2. tworzymy usera z pobranych pol textowych
-             * 3. korzystamy z metody Authenticate z UserService
-             * 4. sprawdzenie, czy haslo jest OK
-             */
+        //linijka powyzej wpisana bezposrednio w konstruktor User userField
+        Alert alert1 = new Alert(Alert.AlertType.INFORMATION, "Zalogowano");
+        Alert alert2 = new Alert(Alert.AlertType.WARNING, "Bledne dane");
+        boolean flag = userService.authenticatie(user);
+        if (flag == true) {
+            alert1.show();
+        } else {
+            alert2.show();
         }
 
+
+        //napisac if'a czy login sie zgadza i czy pass dziala! wyswietlac komunikaty w odpowiednich przypadkach
+        /**
+         * 1. powolac do zycia UserService
+         * 2. tworzymy usera z pobranych pol textowych
+         * 3. korzystamy z metody Authenticate z UserService
+         * 4. sprawdzenie, czy haslo jest OK
+         */
     }
+
+}
 
 
